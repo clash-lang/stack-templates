e9be448bbd3fed1d480cee89560f897b9e5be125
Lucas Bollen <lucas@qbaylogic.com>
2024-01-11 14:03:54 +0100

    Change `Example.Project` to contain a better example
    Co-authored-by: Peter Lebbing <peter@digitalbrains.com>

--- projects/simple-nix/nix/nixpkgs.nix
+++ projects/simple-nix/nix/nixpkgs.nix
@@ -19,6 +19,8 @@ let
           self.callCabal2nix "clash-lib" (sources.clash-compiler + "/clash-lib") {};
         clash-ghc =
           self.callCabal2nix "clash-ghc" (sources.clash-compiler + "/clash-ghc") {};
+        clash-prelude-hedgehog =
+          self.callCabal2nix "clash-prelude" (sources.clash-compiler + "/clash-prelude-hedgehog") {};
         tasty-hedgehog =
           self.callCabal2nix "tasty-hedgehog" sources.tasty-hedgehog {};
         hedgehog =
