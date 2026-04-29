8130e3a62cf6d944afed303270135a69fa807eed
Peter Lebbing <peter@digitalbrains.com>
2026-04-29 17:24:39 +0200

    DECA starter: Change design clock to 40 MHz
    The crystal on the board runs at 50 MHz, a period of 20 ps.
    
    Before, we created a design clock of 20 MHz, a period of 50 ps. And
    named the clock domain and bindings after the period.
    
    This is quite confusing, since the numbers 20 and 50 apply to both
    clocks, and indeed this caused a mistake in one of the comments. The
    comment on the PLL instantiation said it converted a 20 MHz clock into a
    50 MHz clock when it was actually the other way around.
    
    So let's make the design clock 40 MHz, a period of 25 ps. That way you
    can't confuse them as easily. Also, name everything after the frequency
    (`Dom40`, `clk50`, `clk40`). We don't even need to refer to the actual
    period anymore if we use `hzToPeriod` in `createDomain`.

--- projects/deca/src/DECA.hs
+++ projects/deca/src/DECA.hs
@@ -12,13 +12,13 @@ data LedMode
   -- ^ After some period, turn on all disable LEDs, and vice versa
   deriving (Show, Eq, Enum, Generic, NFDataX)
 
--- Define a synthesis domain with a clock with a period of 20000 /ps/.
+-- Define a synthesis domain with a clock with a frequency of 50 MHz.
 -- The signal coming from the reset button is low when pressed, and high when
 -- not pressed, which means we set the reset polarity to active-low.
-createDomain vSystem{vName="Input", vPeriod=20000, vResetPolarity=ActiveLow}
+createDomain vSystem{vName="Input", vPeriod=hzToPeriod 50e6, vResetPolarity=ActiveLow}
 
--- Define a synthesis domain with a clock with a period of 50000 /ps/.
-createDomain vSystem{vName="Dom50", vPeriod=50000}
+-- Define a synthesis domain with a clock with a frequency of 40 MHz.
+createDomain vSystem{vName="Dom40", vPeriod=hzToPeriod 40e6}
 
 {-# ANN deca
   (Synthesize
@@ -41,21 +41,21 @@ deca
       `Annotate` 'StringAttr "chip_pin" "H21"
       `Annotate` 'StringAttr "altera_attribute" "-name IO_STANDARD \"1.5 V Schmitt Trigger\""
   -- ^ Reset signal, straight from KEY0
-  -> Signal Dom50 Bit
+  -> Signal Dom40 Bit
       `Annotate` 'StringAttr "chip_pin" "H22"
       `Annotate` 'StringAttr "altera_attribute" "-name IO_STANDARD \"1.5 V Schmitt Trigger\""
   -- ^ Mode choice, straight from KEY1. See 'LedMode'.
-  -> Signal Dom50 (BitVector 8)
+  -> Signal Dom40 (BitVector 8)
       `Annotate` 'StringAttr "chip_pin" "C5, B4, A5, C4, B7, A6, C8, C7"
       `Annotate` 'StringAttr "altera_attribute" "-name IO_STANDARD \"1.2 V\""
   -- ^ Output containing 8 bits, corresponding to 8 LEDs, LEDs are active-low
   --
   -- Use comma-seperated list in the "chip_pin" attribute to maps the individual
   -- bits of the result to the correct pins on the DECA development kit
-deca clk20 rstBtn modeBtn =
+deca clk50 rstBtn modeBtn =
   exposeClockResetEnable
     (mealy blinkerT initialStateBlinkerT . isRising 1)
-    clk50
+    clk40
     rstSync
     en
     modeBtn
@@ -68,10 +68,10 @@ deca clk20 rstBtn modeBtn =
 
   -- Instantiate a PLL: this stabilizes the incoming clock signal and indicates
   -- when the signal is stable. We're also using it to transform an incoming
-  -- clock signal running at 20 MHz to a clock signal running at 50 MHz.
-  ( clk50 :: Clock Dom50
-    , rstSync :: Reset Dom50
-    ) = altpllSync clk20 rstBtn
+  -- clock signal running at 50 MHz to a clock signal running at 40 MHz.
+  ( clk40 :: Clock Dom40
+    , rstSync :: Reset Dom40
+    ) = altpllSync clk50 rstBtn
 
 -- | Changes the LED mode
 --
@@ -84,14 +84,14 @@ flipMode Rotate = Complement
 flipMode Complement = Rotate
 
 blinkerT
-  :: (BitVector 8, LedMode, Index 6660000)
+  :: (BitVector 8, LedMode, Index 13320000)
   -> Bool
-  -> ((BitVector 8, LedMode, Index 6660000), BitVector 8)
+  -> ((BitVector 8, LedMode, Index 13320000), BitVector 8)
 blinkerT (leds, mode, cntr) key1R = ((leds', mode', cntr'), complement leds)
   where
-    -- clock frequency = 20e6  (20 MHz)
+    -- clock frequency = 40e6  (40 MHz)
     -- led update rate = 333e-3 (every 333ms)
-    cnt_max = maxBound :: Index 6660000 -- 20e6 * 333e-3
+    cnt_max = maxBound :: Index 13320000 -- 40e6 * 333e-3
 
     cntr' | cntr == cnt_max = 0
           | otherwise       = cntr + 1
